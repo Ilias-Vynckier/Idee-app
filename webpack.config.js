96039a73bbbc3f3151e5ee218deeffe7be10dc47
Ilias Vynckier <ilias.vynckier@gmail.com>
2021-09-28 12:12:31 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Progressive Web Application',
+        }),
+        /*new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true,
+          }),*/
+          new WorkboxPlugin.InjectManifest({
+              swSrc:'./src/sw.js',
+          })
+      ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean:true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
