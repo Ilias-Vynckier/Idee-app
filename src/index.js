91561ecf4db489436d3032f09e71f5caaa6ee134
Ilias Vynckier <ilias.vynckier@gmail.com>
2021-10-05 12:17:49 +0200

    added bootstrap, icons, manifest

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import _ from 'lodash';
-import './style.css';
+import _ from "lodash";
+import "./style.css";
+//import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+//const port = process.env.PORT || 3000
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').then(registration => {
-        console.log('SW registered: ', registration);
-      }).catch(registrationError => {
-        console.log('SW registration failed: ', registrationError);
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log("SW registered: ", registration);
+      })
+      .catch((registrationError) => {
+        console.log("SW registration failed: ", registrationError);
       });
-    });
-  }
+  });
+}
+
+function navbar() {
+  const nav = document.createElement("nav");
+  const navtext = document.createElement("span");
+
+  nav.className = "navbar navbar-dark bg-primary";
+  navtext.className="container";
+  navtext.innerHTML = "dink";
+
+  nav.appendChild(navtext);
+
+  return nav;
+}
+
+function containter(){
+  const containter = document.createElement("div");
+  containter.className="container";
+  containter.id="container";
+
+  containter.innerHTML="test";
+  return containter;
+}
+
+document.body.appendChild(navbar());
+document.body.appendChild(containter());
 
 function component() {
-   
+  const element = document.createElement("div");
+  const btn = document.createElement("button");
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  btn.innerHTML = "Click me and check the console!";
+  btn.className = "btn";
+  btn.type="button";
+
+  element.appendChild(btn);
+
+  return element;
 }
+
+document.getElementById("container").appendChild(component());
+//document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
     entry: {
@@ -10,14 +11,32 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'Progressive Web Application',
         }),
-        /*new WorkboxPlugin.GenerateSW({
-            // these options encourage the ServiceWorkers to get in there fast
-            // and not allow any straggling "old" SWs to hang around
-            clientsClaim: true,
-            skipWaiting: true,
-          }),*/
           new WorkboxPlugin.InjectManifest({
               swSrc:'./src/sw.js',
+          }),
+          new WebpackPwaManifest({
+            name: 'Idee app',
+            short_name: 'Idee',
+            description: 'My awesome Progressive Web App!',
+            background_color: '#ffffff',
+            crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+            publicPath: './',
+            icons: [
+                {
+                  src: path.resolve('./src/chicken.png'),
+                  sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+                },
+                {
+                  src: path.resolve('./src/chicken.png'),
+                  size: '1024x1024' // you can also use the specifications pattern
+                },
+                {
+                  src: path.resolve('./src/chicken.png'),
+                  size: '1024x1024',
+                  purpose: 'maskable'
+                }
+              ]
+           
           })
       ],
     output: {
